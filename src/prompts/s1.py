813cfb4ed49df0783487f365a145fe195d316ee3
Greg Peddle <gpeddle@gmail.com>
2023-01-10 07:51:41 -0500

    add-faker

--- src/prompts/s1.py
+++ src/prompts/s1.py
@@ -1,62 +1,77 @@
 """
-write a python program that generates a random list of fake, but reasonable-looking academic papers.
+write a python program that generates a random list of 50000 fake, but reasonable-looking academic papers.
+ - There are 50 possible journals.
+ - Use faker to  create names for 10000 possible authors.
  - Each paper shall have a year, journal, pages, title, authors and optionally may have references to other papers in this list.
  - References are to be in academic format.
  - Any references must be to papers in this list.
  - A paper must not reference itself.
 """
 
-import random
+import faker
+from random import shuffle, randint
 
-# Define papers
+faker_gen = faker.Faker()
+
 papers = []
 
-# Generate a list of random papers
-for i in range(10):
-    # Generate a random year
-    year = random.randint(1990, 2018)
+# Create 50 possible journals
+journals = ["Journal of Applied Mathematics", "Journal of Biomedical Science", "Computer Science Journal",
+"Journal of Analytical Chemistry", "International Journal of Microbiology", "Journal of Chemical Physics",
+"Journal of Geoscience", "Journal of Meteorology", "Journal of Astronomy", "Journal of Neuroscience",
+"Journal of Geology", "Journal of Statistics", "Journal of Molecular Biology", "Journal of Economics",
+"Journal of Genetics", "Journal of History", "Journal of Ecology", "Journal of Geography",
+"Journal of Physics", "Journal of Psychology", "Journal of Sociology", "Journal of Education",
+"Journal of Engineering", "Journal of Law", "Journal of Medicine", "Journal of Technology",
+"Journal of Computer Science", "Journal of Chemistry", "Journal of Biochemistry",
+"Journal of Organic Chemistry", "Journal of Nuclear Chemistry", "Journal of Physics A",
+"Journal of Physics B", "Journal of Physics C", "Journal of Mathematics",
+"Journal of Probability Theory", "Journal of Mathematical Analysis", "Journal of Algebra",
+"Journal of Differential Equations", "Journal of Topology", "Journal of Geometric Analysis",
+"Journal of Number Theory", "Journal of Combinatorics", "Journal of Group Theory",
+"Journal of Representation Theory", "Journal of Graph Theory", "Journal of Logic",
+"Journal of Set Theory", "Journal of Philosophy", "Journal of Political Science",
+"Journal of Anthropology", "Journal of Art History", "Journal of Linguistics",
+"Journal of Literary Studies", "Journal of Musicology", "Journal of Cultural Studies",
+"Journal of Film Studies", "Journal of Theatre Studies", "Journal of Gender Studies",
+"Journal of Media Studies", "Journal of Religion Studies", "Journal of Popular Culture"]
 
-    # Generate a random journal
-    journals = ['Nature', 'Science', 'PLOS', 'Journal of Applied Physics', 'IEEE Transactions on Image Processing']
-    journal = random.choice(journals)
+# Create 10000 possible authors
+authors = []
+for i in range(10000):
+    authors.append(faker_gen.name())
 
-    # Generate a random page number
-    pages = random.randint(1, 200)
-
+# Generate 50000 papers
+for i in range(50000):
+    # Generate a random year between 1950 and 2020
+    year = randint(1950, 2020)
+    # Select a random journal
+    journal = journals[randint(0, len(journals)-1)]
+    # Generate a random number of pages
+    pages = randint(1, 400)
     # Generate a random title
-    title = 'A Study of the Impact of ' + random.choice(
-        ['Climate Change', 'Artificial Intelligence', 'Quantum Computing', 'Advanced Robotics']) + ' on Modern Society'
-
-    # Generate a random set of authors
-    authors = [random.choice(
-        ['John Smith', 'Jane Doe', 'Steven Johnson', 'Anna Baker', 'George Smith', 'Maria Lopez', 'Richard Anderson',
-         'Stephanie Walker'])
-               for i in range(random.randint(1, 5))]
-
-    # Generate a random set of references
-    references = []
-    if len(papers) > 0:
-        num_references = random.randint(0, len(papers))
-        for i in range(num_references):
-            reference = random.choice(papers)
-            if reference['year'] != year:
-                references.append("{}, {}, {}, {}".format(reference['authors'], reference['year'], reference['journal'],
-                                                          reference['title']))
-
+    title = faker_gen.sentence(nb_words=5, variable_nb_words=True)
+    # Generate a random number of authors (up to 5)
+    n_authors = randint(1, 5)
+    authors_list = []
+    for j in range(n_authors):
+        authors_list.append(authors[randint(0, len(authors)-1)])
+    # Generate a random number of references (up to 5)
+    refs = []
+    while len(refs) < 5 and len(papers) > 0:
+        r = randint(0, len(papers)-1)
+        if papers[r]["title"] != title:
+            refs.append(papers[r]["title"])
     # Create the paper
-    paper = {
-        'year': year,
-        'journal': journal,
-        'pages': pages,
-        'title': title,
-        'authors': ", ".join(authors),
-        'references': ", ".join(references)
-    }
-
+    paper = {"year": year,
+             "journal": journal,
+             "pages": pages,
+             "title": title,
+             "authors": authors_list,
+             "refs": refs}
     # Add the paper to the list
     papers.append(paper)
 
-# Print out the papers
+# Print the papers
 for paper in papers:
-    print("{}: {}. {}. {}, {}-{}. {}".format(paper['year'], paper['authors'], paper['title'], paper['journal'],
-                                             paper['pages'], paper['pages'] + 1, paper['references']))
+    print("Title: {}\nYear: {}\nJournal: {}\nPages: {}\nAuthors: {}\nReferences: {}".format(paper["title"], paper["year"], paper["journal"], paper["pages"], ", ".join(paper["authors"]), ", ".join(paper["refs"])))
